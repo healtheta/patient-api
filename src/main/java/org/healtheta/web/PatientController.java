6fa2ee165f0f6c546c28ef4a2c4cefa1d0b04adf
pjeyarat <pragash.jeyaratnam@gmail.com>
2018-10-23 23:01:23 +0530

    added patient controller

--- src/main/java/org/healtheta/web/PatientController.java
+++ src/main/java/org/healtheta/web/PatientController.java
@@ -1,4 +1,25 @@
 package org.healtheta.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/patient")
 public class PatientController {
+    /*@Autowired
+    private PatientRepository patientRepository;
+    @Autowired
+    private IdentifierRepository identifierRepository;*/
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update() {
+        return "updating a patient object";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public String delete() {
+        return "deleting a patient object";
+    }
 }
