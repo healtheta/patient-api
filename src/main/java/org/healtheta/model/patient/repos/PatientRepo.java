d3f4ca17c72821b97dc3d5cbc25a69f5ef07751e
pjeyarat <pragash.jeyaratnam@gmail.com>
2018-10-24 21:21:10 +0530

    added patient repo feature

--- src/main/java/org/healtheta/model/patient/repos/PatientRepo.java
+++ src/main/java/org/healtheta/model/patient/repos/PatientRepo.java
@@ -1,4 +1,21 @@
 package org.healtheta.model.patient.repos;
 
-public class PatientRepo {
+import org.healtheta.model.common.HumanName;
+import org.healtheta.model.common.Reference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.healtheta.model.patient.Patient;
+import org.healtheta.model.common.Identifier;
+import java.util.List;
+
+@Repository
+public interface PatientRepo extends JpaRepository<Patient, Long> {
+    public Patient findPatientById(Long id);
+    public Patient findPatientByIdentifier(Identifier identifier);
+    public List<Patient> findByNameIn(List<HumanName> humanNameList);
+    public List<Patient> findPatientByManagingOrganization(Reference managingOrganization);
+    public List<Patient> findPatientByGeneralPractitioner(Reference generalPractitioner);
+    public List<Patient> findPatientByManagingOrganizationAndGeneralPractitioner(Reference managingOrganization,
+                                                                                        Reference generalPractitioner);
+    public List<Patient> findAll();
 }
