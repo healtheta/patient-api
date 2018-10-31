1a1c5fdac3822babb2a00e41a390edfe676f5e6d
pjeyarat <pragash.jeyaratnam@gmail.com>
2018-10-31 15:37:02 +0530

    fixed inconsistency

--- src/main/java/org/healtheta/web/PatientController.java
+++ src/main/java/org/healtheta/web/PatientController.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping(value = "/")
 public class PatientController {
     @Autowired
     private PatientRepo patientRepo;
